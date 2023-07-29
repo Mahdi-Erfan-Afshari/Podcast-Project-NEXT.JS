92539b69e7aaddf1c5b5951240370561a09438c9
Mahdi Afshar <mahdiafshar2413@gmail.com>
2023-07-29 15:54:23 +0330

    update

--- app/components/Podcast.jsx
+++ app/components/Podcast.jsx
@@ -5,7 +5,7 @@ import Controller from '@/app/components/Controller'
 
 
 async function fetchPodcast() {
-  const response = await fetch(`${server}/api/podcasts`);
+  const response = await fetch(`${server}/api/podcasts`, { cache: 'no-store' });
   // const response = await fetch('https://podcastnextjs.netlify.app/api/podcasts', { cache: 'no-store' });
 
   const podcasts = await response.json();
--- app/components/PodcastsList.jsx
+++ app/components/PodcastsList.jsx
@@ -6,7 +6,7 @@ import {server} from '../api/podcasts/route';
 
 async function fetchPodcasts() {
     // const response = await fetch('https://podcastnextjs.netlify.app/podcasts/api/podcasts', { cache: 'no-store' }, {
-    const response = await fetch(`${server}/api/podcasts`, {
+    const response = await fetch(`${server}/api/podcasts`, { cache: 'no-store' }, {
       next: {
         revalidate: 60 
       }
