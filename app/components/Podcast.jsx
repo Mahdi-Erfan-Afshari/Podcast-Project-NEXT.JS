7596c91ae108e793fb00e6efffc375b9934b88b9
Mahdi Afshar <mahdiafshar2413@gmail.com>
2023-07-29 14:47:04 +0330

    update

--- app/components/Podcast.jsx
+++ app/components/Podcast.jsx
@@ -5,8 +5,8 @@ import Controller from '@/app/components/Controller'
 
 
 async function fetchPodcast() {
-  // const response = await fetch(`${server}/api/podcasts`, { cache: 'no-store' });
-  const response = await fetch('https://podcastnextjs.netlify.app/api/podcasts', { cache: 'no-store' });
+  const response = await fetch(`${server}/api/podcasts`, { cache: 'no-store' });
+  // const response = await fetch('https://podcastnextjs.netlify.app/api/podcasts', { cache: 'no-store' });
 
   const podcasts = await response.json();
   return podcasts;
--- app/podcasts/page.jsx
+++ app/podcasts/page.jsx
@@ -5,7 +5,8 @@ import img1 from '@/app/img/podcast.png';
 import {server} from '../api/podcasts/route';
 
 async function fetchPodcasts() {
-  const response = await fetch('https://podcastnextjs.netlify.app/podcasts/api/podcasts', { cache: 'no-store' }, {
+  // const response = await fetch('https://podcastnextjs.netlify.app/podcasts/api/podcasts', { cache: 'no-store' }, {
+  const response = await fetch(`${server}/api/podcasts`, { cache: 'no-store' }, {
     next: {
       revalidate: 60 
     }
